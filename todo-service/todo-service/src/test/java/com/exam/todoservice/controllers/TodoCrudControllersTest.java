0a801b33496a0e0640566660bb528bb016eb9b8e
Samson <samsonadmasu9@gmail.com>
2023-03-31 19:49:39 +0300

    ++ fix tests

--- todo-service/todo-service/src/test/java/com/exam/todoservice/controllers/TodoCrudControllersTest.java
+++ todo-service/todo-service/src/test/java/com/exam/todoservice/controllers/TodoCrudControllersTest.java
@@ -2,28 +2,19 @@ package com.exam.todoservice.controllers;
 
 import com.exam.todoservice.entity.Todo;
 import com.exam.todoservice.repository.TodoRepository;
-import com.exam.todoservice.service.TodoServices;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-@RunWith(SpringRunner.class)
-@RestClientTest(TodoServices.class)
+
 class TodoCrudControllersTest {
 
     private MockMvc mockMvc;
@@ -31,16 +22,9 @@ class TodoCrudControllersTest {
     @Autowired
     private TodoRepository repository;
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    private MockRestServiceServer server;
-
     @BeforeEach
-    void setUp() throws JsonProcessingException {
+    void setUp() {
         this.mockMvc = MockMvcBuilders.standaloneSetup(new TodoCrudControllers()).build();
-        String detailsString = objectMapper.writeValueAsString( new Todo("Name","name1", 1));
-        this.server.expect(requestTo("/api/basicCruds/todos")).andRespond(withSuccess(detailsString, MediaType.APPLICATION_JSON));
     }
 
     @Test
@@ -57,7 +41,6 @@ class TodoCrudControllersTest {
 
     @Test
     void getAllTodos() {
-
     }
 
     @Test
